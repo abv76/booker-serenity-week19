88d721bd7cd71c1783ab0b269eea01d4affe576f
abv76 <91702855+abv76@users.noreply.github.com>
2022-01-12 10:53:12 +0000

    week-19 HW

---
+++ src/test/java/com/restfulbooker/authinfo/CreatingAuthenticationToken.java
@@ -0,0 +1,30 @@
+package com.restfulbooker.authinfo;
+
+import com.restfulbooker.model.AuthPojo;
+import com.restfulbooker.testbase.TestBaseAuth;
+import io.restassured.http.ContentType;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
+
+//@RunWith(SerenityRunner.class)
+public class CreatingAuthenticationToken extends TestBaseAuth {
+
+    @Title("Creating a new token to use for access to PUT and DELETE booking requests")
+    @Test
+    public void creatingNewToken(){
+        AuthPojo authPojo = new AuthPojo();
+        authPojo.setUsername("admin");
+        authPojo.setPassword("password123");
+        String token = SerenityRest.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(authPojo)
+                .when()
+                .post()
+                .then()
+                .extract()
+                .path("token");
+        System.out.println(token);
+    }
+
+}
